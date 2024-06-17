15457ca11930b1ebd52bb6d1bbaab0e5be773f31
Sofia Danesse <danessesofia@gmail.com>
2024-06-17 15:18:54 -0400

    Commit

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,47 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+    
+
+    useEffect(() => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget,
+        });
+    }, [newBudget, dispatch]);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const value = parseInt(event.target.value, 10);
+
+        if (value > 20000) {
+            alert('Budget cannot exceed £20,000');
+            return;
+        }
+
+        if (value < totalExpenses) {
+            alert(`Budget cannot be less than total allocated spending: £${totalExpenses}`);
+            return;
+        }
+
+        setNewBudget(value);
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: </span>
+            <input 
+                type="number" 
+                step="10" 
+                value={newBudget} 
+                onChange={handleBudgetChange} 
+                max={20000}
+            />
+        </div>
     );
 };
+
 export default Budget;
